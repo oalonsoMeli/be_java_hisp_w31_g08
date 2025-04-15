f5ac2ae2fbdf874b0cf3943de947901aadf07708
jduranmacias_meli <juan.duranmacias@mercadolibre.com.co>
2025-04-15 16:24:07 -0500

    feat: se agrega archivos json que servirán para los proximos servicios

--- src/main/java/com/mercadolibre/socialmeli/repository/UserRepositoryImpl.java
+++ src/main/java/com/mercadolibre/socialmeli/repository/UserRepositoryImpl.java
@@ -1,4 +1,35 @@
 package com.mercadolibre.socialmeli.repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.socialmeli.model.Product;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepositoryImpl implements IUserRepository {
+
+    private List<Product> listOfUsers = new ArrayList<>();
+
+    public UserRepositoryImpl() throws IOException {
+        loadDataBase();
+    }
+
+    public void loadDataBase() {
+        File file;
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Product> users ;
+
+        try{
+            file= ResourceUtils.getFile("classpath:data/users.json");
+            users= objectMapper.readValue(file, new TypeReference<>() {
+            });
+            listOfUsers = users;
+        }catch (Exception exception){
+            throw new RuntimeException("No se pudo parsear el json de usuarios.");
+        }
+    }
 }
