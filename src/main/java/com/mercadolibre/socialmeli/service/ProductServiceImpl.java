ccda4c39416de56b12c2c2f484ffc9c4670281c4
oalonsoMeli <ornella.alonso@mercadolibre.com>
2025-04-16 11:10:24 -0300

    Mi rama

--- src/main/java/com/mercadolibre/socialmeli/service/ProductServiceImpl.java
+++ src/main/java/com/mercadolibre/socialmeli/service/ProductServiceImpl.java
@@ -1,7 +1,9 @@
 package com.mercadolibre.socialmeli.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements IProductService {
-}
+
+  }
