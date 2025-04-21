3c5107c42b6be5c1258d23dc7c41a7a8a370f575
matonelli-meli <manuela.tonelli@mercadolibre.com>
2025-04-21 17:51:12 -0300

    Comentarios swagger

--- src/main/java/com/mercadolibre/socialmeli/controller/ProductController.java
+++ src/main/java/com/mercadolibre/socialmeli/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.mercadolibre.socialmeli.controller;
 import com.mercadolibre.socialmeli.dto.PostDto;
 import com.mercadolibre.socialmeli.dto.PromoPostDto;
 import com.mercadolibre.socialmeli.service.IProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +27,26 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(
+            summary = "Dar de alta una nueva publicación",
+            description = "Permite al vendedor de alta una nueva publicación")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Publicación creada exitosamente."),
+            @ApiResponse(responseCode = "400", description = "Usuario no encontrado")})
     @PostMapping("/post")
     public ResponseEntity<Void> createPost(@RequestBody PostDto postDto) {
         productService.createPost(postDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Listado de publicaciones que un usuario sigue",
+            description = "Permite obtener un listado de las publicaciones realizadas por los vendedores que un usuario sigue en las últimas dos semanas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista exitosamente."),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado"),
+            @ApiResponse(responseCode = "400", description = "Párametro de ordenamiento no permitido"),
+            @ApiResponse(responseCode = "404", description = "No hay publicaciones de quienes sigues.")})
     @GetMapping("/followed/{userId}/list")
     public ResponseEntity<?> getListOfPublicationsByUser(@PathVariable Integer userId,
                                                          @RequestParam(value="order",required = false,
@@ -38,12 +55,26 @@ public class ProductController {
     return new ResponseEntity<>(this.productService.getListOfPublicationsByUser(userId,order), HttpStatus.OK);
     }
 
+
+    @Operation(
+            summary = "Alta publicacion con promoción",
+            description = "Permite llevar a cabo la publicación de un nuevo producto en promoción.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se creo el descuento correctamente"),
+            @ApiResponse(responseCode = "400", description = "Descuento inválido.")})
     @PostMapping("/promo-post")
     public ResponseEntity<String> createPromoPost(@RequestBody PromoPostDto promoPostDto) {
         productService.createPost(promoPostDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+
+    @Operation(
+            summary = "Cantidad de productos en promoción de un vendedor",
+            description = "Permite obtener la cantidad de productos en promoción de un determinado vendedor.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se obtuvo una respuesta exitosa."),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")})
     @GetMapping("/promo-post/count")
     public  ResponseEntity<?> getQuantityOfProducts(@RequestParam Integer user_id){
         return new ResponseEntity<>(productService.getQuantityOfProducts(user_id), HttpStatus.OK);
--- src/main/java/com/mercadolibre/socialmeli/controller/UserController.java
+++ src/main/java/com/mercadolibre/socialmeli/controller/UserController.java
@@ -20,6 +20,12 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Seguir a un vendedor",
+            description = "Permite que un usuario siga a un vendedor determinado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Follow exitoso"),
+            @ApiResponse(responseCode = "400", description = "Usuario no encontrado")})
     @PostMapping("/{userId}/follow/{userIdToFollow}")
     public ResponseEntity<?> followUser(@PathVariable Integer userId, @PathVariable Integer userIdToFollow) {
         userService.followUser(userId, userIdToFollow);
@@ -27,15 +33,26 @@ public class UserController {
 
     }
 
-    //Obtener  un listado de todos los vendedores a los cuales sigue un determinado usuario (¿A quién sigo?)
+    @Operation(
+            summary = "Listado de todos los vendedores que sigue un usuario",
+            description = "Permite obtener  un listado de todos los vendedores a los cuales sigue un determinado usuario (¿A quién sigo?)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista exitosamente"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")})
     @GetMapping ("/{userId}/followed/list")
-
     public ResponseEntity<?> getFollowed(@PathVariable Integer userId,
                                          @RequestParam(value = "order", required = false) String order){
         return new ResponseEntity<>(userService.searchFollowedSellers(userId,order), HttpStatus.OK);
-
     }
 
+    @Operation(
+            summary = "Cantidad de usuarios que siguen a un vendedor",
+            description = "Permite obtener el resultado de la cantidad de usuarios que siguen a un determinado vendedor"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Respuesta exitosa"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     @GetMapping("/{userId}/followers/count")
     public ResponseEntity<?> getFollowersCountByUserId(@PathVariable Integer userId){
         return new ResponseEntity<>(this.userService.getFollowersCountByUserId(userId), HttpStatus.OK);
@@ -58,6 +75,12 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            summary = "Listado de todos los usuarios que siguen a un vendedor",
+            description = "Obtener un listado de todos los usuarios que siguen a un determinado vendedor (¿Quién me sigue?)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista exitosamente"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")})
     @GetMapping("/{userId}/followers/list")
     public ResponseEntity<FollowersDto> getFollowers(@PathVariable Integer userId,
                                                     @RequestParam(value = "order", required = false) String order){
