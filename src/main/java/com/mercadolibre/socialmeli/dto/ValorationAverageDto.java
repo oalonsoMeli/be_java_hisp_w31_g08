084d8f83acc78c39b4576765c32e375f88e656b6
oalonsoMeli <ornella.alonso@mercadolibre.com>
2025-04-22 13:32:16 -0300

    feat: bonus de valoraciones

---
+++ src/main/java/com/mercadolibre/socialmeli/dto/ValorationAverageDto.java
@@ -0,0 +1,14 @@
+package com.mercadolibre.socialmeli.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ValorationAverageDto {
+    Double average;
+}
