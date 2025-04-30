ab6f1fe6e7a373c14e6367787fa9cf47a1979e02
oalonsoMeli <ornella.alonso@mercadolibre.com>
2025-04-30 11:43:10 -0300

    feat: sigo trabajando en test

--- src/test/java/com/mercadolibre/socialmeli/service/UserServiceImplTest.java
+++ src/test/java/com/mercadolibre/socialmeli/service/UserServiceImplTest.java
@@ -53,4 +53,22 @@ class UserServiceImplTest {
         assertEquals("Abril", result.getFollowed().get(0).getUser_name());
         assertEquals("Orne", result.getFollowed().get(1).getUser_name());
     }
+
+    @Test
+    void searchFollowedSellers_withOrderDesc_shouldReturnTheUsersNameWithOrderDesc(){
+        User user1 = new User(1, "Orne", "orne@example.com", Set.of());
+        User user3 = new User(3, "Abril", "abril@example.com", Set.of());
+        User user2 = new User(2, "Yoa", "yoa@example.com", Set.of(1, 3)); // Sigue a user1 y user3
+
+        when(repository.getUserById(2)).thenReturn(Optional.of(user2));
+        when(repository.findUsersById(List.of(1, 3))).thenReturn(List.of(user1, user3));
+
+        // Act
+        FollowedDto result = service.searchFollowedSellers(2, "name_desc");
+
+        // Assert
+        assertEquals(2, result.getFollowed().size());
+        assertEquals("Orne", result.getFollowed().get(0).getUser_name());
+        assertEquals("Abril", result.getFollowed().get(1).getUser_name());
+    }
 }
